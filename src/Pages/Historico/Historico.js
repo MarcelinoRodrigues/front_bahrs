8ea42907d5e738d5b06e53b62af6e67ff0de96a3
Marcelino Rodrigues <marcelino.rodrigues.d.n@gmail.com>
2023-05-19 13:25:14 -0300

    adicionando filtragem na aplicação de historico

--- src/Pages/Historico/Historico.js
+++ src/Pages/Historico/Historico.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { Table, TBody, TD, TH, THead, TR } from '../../styles/styles';
 import Nav from '../../components/Nav';
 import moment from 'moment';
+import { Button, Dropdown, FilterContainer, Input } from './styles';
 
 export default function Historico() {
     const [data, setData] = useState([]);
@@ -15,9 +16,67 @@ export default function Historico() {
         fetchData();
     }, []);
 
+    const MyEnum = {
+        0: 'Entrada',
+        1: 'Saida',
+        2: 'Vaga',
+        3: 'Placa',
+        4: 'Funcionario',
+        5: 'Valor',
+        6: 'Limpeza',
+    };
+
+    const [selectedOption, setSelectedOption] = useState('');
+    const [inputValue, setInputValue] = useState('');
+
+    const handleDropdownChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const url = `https://localhost:5001/api/Historico/Filter`;
+
+        try {
+            const response = await axios.get(url, {
+                params: {
+                    search: inputValue,
+                    enumBusca: selectedOption
+                }
+            });
+
+            const filteredData = response.data;
+            setData(filteredData);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <div className="main">
-            <Nav/>
+            <Nav />
+            <FilterContainer>
+                <Dropdown value={selectedOption} onChange={handleDropdownChange}>
+                    <option value="">Selecione uma opção</option>
+                    {Object.values(MyEnum).map((option) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Dropdown>
+                <Input
+                    type="text"
+                    value={inputValue}
+                    onChange={handleInputChange}
+                    placeholder="Informe um valor"
+                />
+                <Button onClick={handleSubmit}>Pesquisar</Button>
+            </FilterContainer>
             <Table>
                 <THead>
                     <tr>
